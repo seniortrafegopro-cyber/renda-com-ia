1896233e818312e00f3059a8b0f1d51cae24dbfd
seniortrafegopro-cyber <seniortrafegopro@gmail.com>
2026-09-08 19:10:44 -0300

    Ativa checkout oficial da Rota da Entrevista

--- src/config.js
+++ src/config.js
@@ -1,3 +1,2 @@
-// Insira aqui somente o checkout do produto “Rota da Entrevista” por R$ 19,90.
-// O link anterior foi removido porque ainda apontava para outro produto de R$ 37.
-export const CHECKOUT_URL = '';
+// Checkout oficial do produto “Rota da Entrevista” por R$ 19,90.
+export const CHECKOUT_URL = 'https://pay.kiwify.com.br/EpsMXjK';
