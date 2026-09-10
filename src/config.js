398d458ecba03ca153d8101b66e183133ba7971a
seniortrafegopro-cyber <seniortrafegopro@gmail.com>
2026-09-10 19:35:48 -0300

    Atualiza oferta para R$ 4,90

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// Checkout oficial do produto “Rota da Entrevista” por R$ 19,90.
+// Checkout oficial do produto “Rota da Entrevista” por R$ 4,90.
 export const CHECKOUT_URL = 'https://pay.kiwify.com.br/EpsMXjK';
