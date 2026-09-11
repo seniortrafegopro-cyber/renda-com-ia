5f3af98db7c030e6140b10b549fcc57cf17a1fd8
seniortrafegopro-cyber <seniortrafegopro@gmail.com>
2026-09-11 09:49:14 -0300

    Atualiza oferta da Rota para R$ 37,00

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// Checkout oficial do produto “Rota da Entrevista” por R$ 5,00.
+// Checkout oficial do produto “Rota da Entrevista” por R$ 37,00.
 export const CHECKOUT_URL = 'https://pay.kiwify.com.br/EpsMXjK';
--- src/tracking.js
+++ src/tracking.js
@@ -10,7 +10,7 @@ const EVENTS = [
 ];
 const META_PIXEL_ID = '1083842784036393';
 const PRODUCT_NAME = 'Rota da Entrevista';
-const PRODUCT_VALUE = 5;
+const PRODUCT_VALUE = 37;
 const UPSELL_NAME = 'LinkedIn Estratégico';
 const UPSELL_VALUE = 29.9;
 
